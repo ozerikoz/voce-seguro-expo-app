acf5b2b3d51ace68d44038e1d8a343174a2efbaa
Julio-CRodrigues <juliocesarccr29@gmail.com>
2025-05-11 00:21:53 -0300

    Atualziações de versão

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
-import { Stack } from 'expo-router/stack';
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
+export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
