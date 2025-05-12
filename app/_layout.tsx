1be007dd44e24bc7ed6198553f0f160992235cde
Julio-CRodrigues <juliocesarccr29@gmail.com>
2025-05-11 23:44:16 -0300

    alterado imagens e ajustado splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,38 @@
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      // Simula carregamento de recursos
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      setAppIsReady(true);
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </View>
   );
 }
